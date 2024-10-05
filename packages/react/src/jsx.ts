261f42da04ad201ad684f4ceaffb4902045ff3d2
Edwin <changliu@192.168.0.107>
2024-10-05 11:22:59 +0800

    feat: react 代码

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -1,63 +1,109 @@
-// ReactElement
-import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbols'
-import { ReactElementType, Type, Key, Props, Ref } from 'shared/ReactTypes'
+import { REACT_ELEMENT_TYPE, REACT_FRAGMENT_TYPE } from 'shared/ReactSymbols';
+import {
+	Type,
+	Key,
+	Ref,
+	Props,
+	ReactElementType,
+	ElementType
+} from 'shared/ReactTypes';
 
-// ReactElement 构造函数
-const ReactElement = (
+// ReactElement
+
+const ReactElement = function (
 	type: Type,
 	key: Key,
 	ref: Ref,
 	props: Props
-): ReactElementType => {
+): ReactElementType {
 	const element = {
-		$$typeof: REACT_ELEMENT_TYPE, // 指明当前数据结构是 react
+		$$typeof: REACT_ELEMENT_TYPE,
+		type,
 		key,
 		ref,
 		props,
-		type,
-		_mark: 'Edwin' // 区别于真实 react 项目中的 element
-	}
-	return element
+		__mark: 'KaSong'
+	};
+	return element;
+};
+
+export function isValidElement(object: any) {
+	return (
+		typeof object === 'object' &&
+		object !== null &&
+		object.$$typeof === REACT_ELEMENT_TYPE
+	);
 }
 
-export const jsx = (type: Type, config: any, ...maybeChildren: any) => {
-	let key: Key = null // key 默认为 null
-	const props: Props = {}
-	let ref: Ref = null
+export const createElement = (
+	type: ElementType,
+	config: any,
+	...maybeChildren: any
+) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
 
-	// eslint-disable-next-line guard-for-in
 	for (const prop in config) {
-		const val = config[props]
+		const val = config[prop];
 		if (prop === 'key') {
 			if (val !== undefined) {
-				key = `${val}` // key 转成字符串
+				key = '' + val;
 			}
-			continue
+			continue;
 		}
 		if (prop === 'ref') {
 			if (val !== undefined) {
-				ref = val
+				ref = val;
 			}
-			continue
+			continue;
 		}
-		// prop 如果是自己 config 上的，而不是原型上的，则赋值
 		if ({}.hasOwnProperty.call(config, prop)) {
-			props[prop] = val
+			props[prop] = val;
 		}
 	}
-
-	const maybeChildrenLength = maybeChildren.length
+	const maybeChildrenLength = maybeChildren.length;
 	if (maybeChildrenLength) {
-		// 如果有长度，则讨论 children 的情况
 		if (maybeChildrenLength === 1) {
-			props.children = maybeChildren[0] // child 是一个 reactelement
+			props.children = maybeChildren[0];
 		} else {
-			props.children = maybeChildren // child 是 reactelement 数组
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const Fragment = REACT_FRAGMENT_TYPE;
+
+export const jsx = (type: ElementType, config: any, maybeKey: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	if (maybeKey !== undefined) {
+		key = '' + maybeKey;
+	}
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
 		}
 	}
 
-	return ReactElement(type, key, ref, props) // 经过处理之后，最终返回一个新的 reactElement
-}
+	return ReactElement(type, key, ref, props);
+};
 
-// 实际 react 中，jsx 在开发环境会多做一些额外的检查，但这里省略掉了
-export const jsxDev = jsx
+export const jsxDEV = jsx;
--- packages/shared/ReactSymbols.ts
+++ packages/shared/ReactSymbols.ts
@@ -1,6 +1,29 @@
-// 为了防止别人滥用 ReactElement，所以需要定义成独一无二的值
-const supportSymbol = typeof Symbol === 'function' && Symbol.for // 判断当前宿主环境是否支持 Symbol
+const supportSymbol = typeof Symbol === 'function' && Symbol.for;
 
 export const REACT_ELEMENT_TYPE = supportSymbol
 	? Symbol.for('react.element')
-	: 0xeac7
+	: 0xeac7;
+
+export const REACT_FRAGMENT_TYPE = supportSymbol
+	? Symbol.for('react.fragment')
+	: 0xeaca;
+
+export const REACT_CONTEXT_TYPE = supportSymbol
+	? Symbol.for('react.context')
+	: 0xeacc;
+
+export const REACT_PROVIDER_TYPE = supportSymbol
+	? Symbol.for('react.provider')
+	: 0xeac2;
+
+export const REACT_SUSPENSE_TYPE = supportSymbol
+	? Symbol.for('react.suspense')
+	: 0xead1;
+
+export const REACT_LAZY_TYPE = supportSymbol
+	? Symbol.for('react.lazy')
+	: 0xead4;
+
+export const REACT_MEMO_TYPE = supportSymbol
+	? Symbol.for('react.memo')
+	: 0xead3;
